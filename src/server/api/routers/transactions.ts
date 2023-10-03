0c2fdbfd66da5ef3c7998955bd0298b4e9874425
Andy Demma <rdemma.andy@gmail.com>
2023-10-03 18:07:56 -0500

    Complete add transaction Prisma mutation

--- src/server/api/routers/transactions.ts
+++ src/server/api/routers/transactions.ts
@@ -30,19 +30,35 @@ export const transactionsRouter = createTRPCRouter({
   create: privateProcedure.input(
     z.object({
       transactionNumber: z.number(),
-      vendor: z.string(),
-      glCode: z.string(),
+      vendorId: z.string(),
+      glCodeId: z.string(),
       amount: z.number(),
       date: z.string(),
     })
   )
   .mutation(async ({ ctx, input }) => {
 
-    const { success } = await ratelimit.limit(authorId);
-    if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+    const ifVendorExists = await ctx.db.vendor.findFirst({
+      where: { id : input.vendorId },
+    })
+
+    const ifGLCodeExists = await ctx.db.generalLedgerCode.findFirst({
+      where: { id : input.glCodeId },
+    })
+
+    if(ifVendorExists || !ifGLCodeExists) throw new TRPCError({ code: "NOT_FOUND"});
 
     const transaction = await ctx.db.transaction.create({
       data: {
+        transactionNumber: input.transactionNumber,
+        vendor: {
+          connect: {id: input.vendorId },
+        },
+        gLCode: {
+          connect: {id: input.glCodeId},
+        },
+        amount: input.amount,
+        date: input.date,
       },
     });
 
